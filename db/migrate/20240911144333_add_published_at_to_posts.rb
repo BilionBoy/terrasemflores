05d66a75f7db8c5f21c23090f6f30769c4dbd50b
BilionBoy <dhiogoandre26@gmail.com>
2024-09-15 23:27:46 -0400

    Descrição das alterações realizadas

--- db/migrate/20240911144333_add_published_at_to_posts.rb
+++ db/migrate/20240911144333_add_published_at_to_posts.rb
@@ -1,4 +1,4 @@
-class AddPublishedAtToPosts < ActiveRecord::Migration[7.1]
+class AddPublishedAtToPosts < ActiveRecord::Migration[6.0]
   def change
     add_column :posts, :published_at, :datetime
   end
